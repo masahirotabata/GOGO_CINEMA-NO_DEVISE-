8354a1012b6fa3c9c159d42e74efe2f832ad64ac
Masahiro Tabata <camusxyz1202@gmail.com>
2022-03-18 16:55:16 +0000

    更新

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -6,7 +6,7 @@ class Public::FavoritesController < ApplicationController
     flash[:notice] = "いいねしました"
     redirect_to request.referer
   end
-
+  #更新
   def destroy
     @movie_comment = MovieComment.where(movie_comment_id: params[:movie_comment_id])
     favorite = current_customer.favorites.find_by(movie_comment_id: @movie_comment.id)
